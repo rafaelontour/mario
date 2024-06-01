0f531fd4d40fd408adc29fedb732f182c7b8e524
rafaelnargolo <124110743+rafaelnargolo@users.noreply.github.com>
2024-06-01 19:02:50 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,110 @@
+
+var mario = document.querySelector('#mario');
+var barril = document.querySelector('#pipe');
+var posicaoBottomMario = +window.getComputedStyle(mario).bottom.replace('px', '');
+var botaoReiniciar = document.querySelector('#botao-iniciar-reiniciar');
+var pontos = document.querySelector('#pontos-numero');
+var pontuacao = 0;
+
+const audio = new Audio('audio.mp3');
+
+
+window.onload = () => {
+    audio.play();
+    audio.currentTime = 0.4;
+}
+
+botaoReiniciar.addEventListener("click", () => {
+    window.location.reload();    
+});
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') {
+        mario.classList.add('jump');
+    }
+    
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 498);
+});
+
+var somarPontos = setInterval(() => {
+    var posicaoBarril = +window.getComputedStyle(barril).left.replace('px', '');
+    
+    console.log(posicaoBarril);
+    
+    if (mario.classList.contains('soma-ponto')) {
+        pontuacao = pontuacao + 50;
+        pontos.innerHTML = pontuacao;
+        console.log("PONTUOU");
+        mario.classList.remove('soma-ponto');
+    }
+    
+}, 1500);
+
+var loop = setInterval(() => {
+    var posicaoBottomMario = +window.getComputedStyle(mario).bottom.replace('px', '');
+    var posicaoBarril = +window.getComputedStyle(barril).left.replace('px', '');
+    
+    console.log(audio.currentTime);
+    
+    if (posicaoBottomMario > 40) {
+        mario.src = 'imagens/mario-voando.png';
+        mario.style.left = '-50px';
+        mario.style.width = '220px';
+
+        if (posicaoBottomMario < 41.5) { 
+            mario.src = 'imagens/mario.gif';
+            mario.style.left = '0px';
+            mario.style.width = '120px';
+            
+            mario.classList.add('soma-ponto');
+        }
+        
+    }
+
+    if (audio.currentTime > 84) {
+        audio.currentTime = 5.57;
+    }
+    
+    if (posicaoBarril <= 105 && posicaoBottomMario < 110 && posicaoBarril > 0) {
+        var imgsChao = document.querySelectorAll('.c-um, .c-dois');
+        var nuvens = document.querySelectorAll('.nuvem');
+        var arbustos = document.querySelectorAll('.arbusto');
+
+        mario.classList.remove('soma-ponto');
+        pontos.innerHTML = pontuacao;
+        
+        barril.style.animationPlayState = 'paused';    
+        mario.style.animation = 'none';
+        
+        nuvens.forEach(nuvem => {
+            nuvem.style.animationPlayState = 'paused';
+        });
+        
+        imgsChao.forEach(chao => {
+            chao.style.animationPlayState = 'paused';
+        });
+
+        arbustos.forEach(arbusto => {
+            arbusto.style.animationPlayState = 'paused';
+        });     
+        
+        mario.src = '../imagens/game-over.png';
+        mario.style.bottom = `${posicaoBottomMario}px`;
+        mario.style.left = `50px`;
+        mario.style.width = '60px';
+        mario.style.animation = 'game-over 1s ease-out 1s forwards';
+
+        botaoReiniciar.style.color = "#2C2316";
+
+        const audioPerdeu = new Audio('perdeu.mp3');
+
+        audio.pause();
+        audioPerdeu.play();
+
+        clearInterval(loop);
+        clearInterval(somarPontos);
+    } 
+}, 0.1);
