28b7e15c3e88ba975717aa00b09f2789049351dd
rafaelnargolo <124110743+rafaelnargolo@users.noreply.github.com>
2024-06-01 19:57:40 -0300

    Botão pular

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ botaoReiniciar.addEventListener("click", () => {
     window.location.reload();    
 });
 
-document.getElementById('pular').addEventListener('touchstart', () => {
+document.getElementById('pular').addEventListener('touchend', () => {
     if (!pulando) {
         pulando = true;
         mario.classList.add('jump');
